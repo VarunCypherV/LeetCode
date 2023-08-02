db3b3b7fde78d25f8d58b86659421e313e9b5250
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-02 10:34:54 +0530

    Update ValidAnagram.cpp

--- ValidAnagram.cpp
+++ ValidAnagram.cpp
@@ -1,4 +1,4 @@
-//HASHMAP
+//HASHMAP , RUN TIME 16MS
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -7,15 +7,20 @@ public:
             string ch21(1, ch);
             hashmap1[ch21]++;
         }
-
         for(char ch2:t){
             string ch22(1, ch2);
             if(hashmap1[ch22]){
                 hashmap1[ch22]--;
-            }
+            }     
             else{
                 return false;
             }
+        }
+        for(char ch3 : s ){
+            string ch23(1, ch3);
+            if(hashmap1[ch23]!=0){
+                return false;
+        } 
         }
         return true;
     }
