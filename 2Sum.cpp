eb22ebdb8259e2d09480f79dd401f702fba478b1
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-03 00:45:18 +0530

    Update 2Sum.cpp

--- 2Sum.cpp
+++ 2Sum.cpp
@@ -16,3 +16,38 @@ public:
         return result;
     }
 };
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector <int> result;
+        int equal_k=0;
+        int equal_kf=0;
+        for(int i=0;i<nums.size();i++){
+            auto key=target-nums[i];
+            auto key_ind=find(nums.begin(),nums.end(),key);
+            auto keyf=key_ind - nums.begin();
+            if(i!=keyf){
+                if(key_ind!=nums.end()){
+                result.push_back(i);
+                result.push_back(keyf);
+                return result;
+                }
+            }
+            else{
+                equal_k=i;
+                equal_kf=keyf;
+            }
+            }
+            
+     if(equal_k!=0){
+            result.push_back(equal_k);
+            result.push_back(equal_kf);
+     }        
+     return result;
+        }
+};
+
+
