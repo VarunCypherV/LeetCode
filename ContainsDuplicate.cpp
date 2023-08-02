5c32962875732d2dd53adbfc328f7ca09adbf15c
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-02 09:04:05 +0530

    Update ContainsDuplicate.cpp

--- ContainsDuplicate.cpp
+++ ContainsDuplicate.cpp
@@ -14,3 +14,18 @@ public:
     return false;
     }
 };
+
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int length=nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i=0 ; i<length-1; i++){
+                if(nums[i] == nums[i+1]){
+                    return true;
+                }
+        }
+    return false;
+    }
+};
