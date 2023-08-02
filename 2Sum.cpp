8639bef8c7540001166d5a0c3111d541b3ab00b9
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-03 00:52:43 +0530

    Update 2Sum.cpp

--- 2Sum.cpp
+++ 2Sum.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+        return {}; // No solution found
+    }
+};
+
+//abv is best soln , below is what i cud manage
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
         vector <int> result;
         for(int i=0;i<nums.size();i++){
             int res=target-nums[i];
