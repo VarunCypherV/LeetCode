7a7e335c65ac760cf8e2f5e7ce80eea213c82bd1
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-03 20:59:21 +0530

    Update longestconseSequence.cpp

--- longestconseSequence.cpp
+++ longestconseSequence.cpp
@@ -14,10 +14,8 @@ public:
         cout<<endl;
         for(int i=1;i<nums.size();i++){
             if(nums[i]==nums[i-1]+1){
-                count++;
-                
+                count++;            
                 if(i==nums.size()-1){flag=2;}
-                cout<<nums[i]<<"x"<<nums[i-1]<<"if "<<count<<endl;
                 continue;
             }
             else{
@@ -25,7 +23,6 @@ public:
                     flag=1;
                     max=count+1;
                 } 
-                cout<<nums[i]<<"x"<<nums[i-1]<<"else "<<count<<max<<endl;
                 count=0;
                 
             }
@@ -33,14 +30,9 @@ public:
         if(!nums.size()>0){
             return 0;
         }
-        if(flag==0 && count>=max){
+        if((flag==0 || flag==2)&& count>=max){
                max=count+1;
-               cout<<count<<"x"<<max<<"outerif "<<count<<endl;
         }   
-        if(flag==2 && count>=max){
-            max=count+1;
-            cout<<"flag2 :" <<max;
-        }     
         return max;
     }
 };
