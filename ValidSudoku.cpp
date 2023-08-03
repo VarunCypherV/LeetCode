60c2d2d7c9c284867467f7248e341b06b3304ea2
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-03 19:05:04 +0530

    Update ValidSudoku.cpp

--- ValidSudoku.cpp
+++ ValidSudoku.cpp
@@ -1,3 +1,60 @@
+// best optimization : array not hashmap 
+class Solution {
+public:
+    bool isValid(vector<vector<char>>& board, int row, int flag) { //flag=1 row, flag=0 col
+        int occurrences[10] = {0};
+        for (int j = 0; j < 9; j++) {
+            char current = (flag == 1) ? board[row][j] : board[j][row];
+            if (current == '.') {
+                continue;
+            }
+            int digit = current - '0';
+            if (occurrences[digit] == 0) {
+                occurrences[digit]++;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool isValidSubgrid(vector<vector<char>>& board, int startRow, int startCol) {
+        int occurrences[10] = {0};
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                char current = board[i][j];
+                if (current == '.') {
+                    continue;
+                }
+                int digit = current - '0'; // converts into integer
+                if (occurrences[digit] == 0) {
+                    occurrences[digit]++;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; i++) {
+            if (!isValid(board, i, 1) || !isValid(board, i, 0)) {
+                return false;
+            }
+        }
+        for (int box = 0; box < 9; box++) {
+            int startRow = (box / 3) * 3;
+            int startCol = (box % 3) * 3;
+            if (!isValidSubgrid(board, startRow, startCol)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+
+//not the max optimization hashmap
 class Solution {
 public:
     bool isValid(vector<vector<char>>& board, int row, int flag) { //flag=1 row, flag=0 col
