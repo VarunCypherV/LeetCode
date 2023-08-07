a49ad00ed1e5892aff73eb7e9ec184234de1596a
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-07 21:49:21 +0530

    Update validParenthesisStack.cpp

--- validParenthesisStack.cpp
+++ validParenthesisStack.cpp
@@ -1,3 +1,33 @@
+//best 100percent beat
+class Solution {
+public:
+    bool isValid(string s) {
+        vector<string> stack{"$"};
+        unordered_map<string, string> hmap;
+        hmap["("] = ")";
+        hmap["{"] = "}";
+        hmap["["] = "]";
+  
+        for (auto x : s) {
+            string l = stack.back();
+            if (l == "$") {
+                stack.push_back(string(1, x));
+            } else {
+                stack.pop_back();
+                string h(1, x);
+                //l is stack abck , h is current
+                if( !((h==")" && l=="(")||(h=="]" && l=="[")||(h=="}" && l=="{"))){
+                     stack.push_back(l);
+                    stack.push_back(h);
+                }
+            }
+        }
+        return stack.size() == 1; 
+    }
+};
+
+
+//second soln hashmap by me 
 class Solution {
 public:
     bool isValid(string s) {
