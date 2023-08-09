73da3bb89e81bf7c1684acdd774d06c5fe19f54b
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-09 23:36:11 +0530

    Update MinStack.cpp

--- MinStack.cpp
+++ MinStack.cpp
@@ -1,6 +1,49 @@
 // Min Stack
 
+//Using array pointer
+class MinStack {
 
+private:     
+    int* array;
+    int tops;
+public:
+    MinStack() {
+        tops=-1;
+        array = new int[10000];
+    }
+    
+    void push(int val) {
+        tops++;
+        *(array+tops)=val;
+    }
+    
+    void pop() {
+         tops--;
+    }
+    
+    int top() {
+        return *(array+tops);
+    }
+    
+    int getMin() {
+        int min=INT_MAX;
+        for(int i=0;i<tops+1;i++){
+            if(*(array+i)<min){
+                min=*(array+i);
+            }
+        }
+        return min;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
 
 
 // using vector
