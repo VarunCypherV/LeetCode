a08be49507af9b43440cdf12f6792f2f791df0a4
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-10 02:07:26 +0530

    Update EvaluatePostfixorReversePolish.cpp

--- EvaluatePostfixorReversePolish.cpp
+++ EvaluatePostfixorReversePolish.cpp
@@ -25,3 +25,33 @@ public:
         return stack.top();
     }
 };
+
+
+//question said trunc towrds zero in div
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> stack; 
+        for (auto x : tokens) {
+            if (x == "+" || x == "-" || x == "*" || x == "/") {
+                int b = stack.top();
+                stack.pop();
+                int a = stack.top();
+                stack.pop();
+                if (x == "+") {
+                    stack.push(a + b);
+                } else if (x == "-") {
+                    stack.push(a - b);
+                } else if (x == "*") {
+                    stack.push(a * b);
+                } else if (x == "/") {
+                    stack.push(int(a / b));
+                }
+            } else {
+                stack.push(stoi(x));
+            }
+        }
+        
+        return stack.top();
+    }
+};
