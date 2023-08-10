86be41fb2ebd7cdac251e053bf8b57f023169f2c
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-10 17:35:12 +0530

    Update DailyTemperatureStack.cpp

--- DailyTemperatureStack.cpp
+++ DailyTemperatureStack.cpp
@@ -1,4 +1,3 @@
-//CONCEPT OF REVERSE THE GIVEN VECTOR AND SUE STACK
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
@@ -17,3 +16,9 @@ public:
         return answer;
     }
 };
+
+//intuituion : local maximum 
+// the while index not empty and temp check : it checks if incoming element is greater than or eq current top if yes pops existing top 
+// then it measures distance of current element wrt the existing 
+// pushes it self
+// every iteration checks for local max then measures distance then pushes itself as it can be a local maximum for next element as well , its a stack of indexes of all local maximas
