66dc25b71e0387619451a63b428c1ef2521ca7f0
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-13 18:06:02 +0530

    Create BinarySearch.cpp

---
+++ BinarySearch.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    int BinarySearch(vector<int>& nums, int target , int l , int r){
+    
+        int m = l + (r-l)/2;
+        if(l>r){
+            return -1;
+        }
+        if(nums[m]==target){
+            return m;
+        }
+        else if(nums[m]>target){
+            return BinarySearch(nums,target,l,m-1);
+        }
+        else if(nums[m]<target){
+            return BinarySearch(nums,target,m+1,r);
+        }
+        return -1; //if not found
+        
+    }
+
+    int search(vector<int>& nums, int target) {
+           int l=0;
+           int r=nums.size()-1;
+           return BinarySearch(nums,target,l,r);
+    }
+};
