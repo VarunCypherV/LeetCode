1a3967c7a46e63536fe3fab1b7f2fbc8c79a2666
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-14 13:49:30 +0530

    Update FindMinimumInRoatatedSortArrayBS.cpp

--- FindMinimumInRoatatedSortArrayBS.cpp
+++ FindMinimumInRoatatedSortArrayBS.cpp
@@ -16,3 +16,21 @@ public:
         return BinarySearch(nums, 0, nums.size() - 1);
     }
 };
+
+
+//NOTE BELOW IS A BS SOLN WITHOUT RECURSION
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+};
