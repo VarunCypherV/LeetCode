b6b7af02d11b45a9824e4dcab14c5df4ebb6fb32
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-14 13:46:51 +0530

    Create FindMinimumInRoatatedSortArrayBS.cpp

---
+++ FindMinimumInRoatatedSortArrayBS.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int BinarySearch(vector<int>& nums, int l, int r) {
+        if (l >= r) {
+            return nums[l];
+        }
+        int m = l + (r - l) / 2;
+        if (nums[m] > nums[r]) {
+            return BinarySearch(nums, m + 1, r);
+        } else {
+            return BinarySearch(nums, l, m);
+        }
+    }
+
+    int findMin(vector<int>& nums) {
+        return BinarySearch(nums, 0, nums.size() - 1);
+    }
+};
