e1ddd90f2fa59974ee55635ce00abb0236e6e904
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-20 15:25:04 +0530

    Update TimeMapBinarySearchImportant.cpp

--- TimeMapBinarySearchImportant.cpp
+++ TimeMapBinarySearchImportant.cpp
@@ -1,35 +1,35 @@
+//concept correct
 class TimeMap {
 private:
-    unordered_map<int, vector<pair<string,string>>> storage;
+    unordered_map<string, vector<pair<int,string>>> storage;
 public:
     TimeMap() {
         
     }
-    string BinarySearch(vector<pair<string,string>>& nums, string target , int l , int r){
-    
-        int m = l + (r-l)/2;
-        if(l>r){
-            return "";
+    string BinarySearch(vector<pair<int, string>>& nums, int target, int l, int r) {
+    if (l > r) {
+        if (r >= 0) {
+            return nums[r].second;  // if value at all there return if no match :)
         }
-        if(nums[m].first==target){
-            return nums[m].second;
-        }
-        else if(nums[m].first>target){
-            return BinarySearch(nums,target,l,m-1);
-        }
-        else{
-            return BinarySearch(nums,target,m+1,r);
-        }
-        return ""; //if not found
-        
+        return "";
     }
+    int m = l + (r - l) / 2;
+    if (nums[m].first == target) {
+        return nums[m].second;
+    } else if (nums[m].first > target) {
+        return BinarySearch(nums, target, l, m - 1);
+    } else {
+        return BinarySearch(nums, target, m + 1, r);
+    }
+}
+
     void set(string key, string value, int timestamp) {
-        storage[timestamp].push_back({key, value});
-        sort(storage[timestamp].begin(), storage[timestamp].end());
+        storage[key].push_back({timestamp, value});
+        sort(storage[key].begin(), storage[key].end());
 }
     
     string get(string key, int timestamp) {
-        return BinarySearch(storage[timestamp],key,0,storage[timestamp].size()-1);
+        return BinarySearch(storage[key],timestamp,0,storage[key].size()-1);
     }
 };
 
