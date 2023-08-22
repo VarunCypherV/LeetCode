587e63e3d2dab0219bfa5c5fdd8ed5db0d89ba45
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-22 19:24:04 +0530

    Update SlidingWindowMaximumSW.cpp

--- SlidingWindowMaximumSW.cpp
+++ SlidingWindowMaximumSW.cpp
@@ -1,3 +1,62 @@
+//NEED TO REVIEW NEW DATA STRUCTURES
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> result;
+        deque<int> dq; // Store indices of elements
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            // Remove indices of elements that are out of the current window
+            while (!dq.empty() && dq.front() <= i - k) {
+                dq.pop_front();
+            }
+            
+            // Remove indices of elements that are smaller than the current element
+            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+            
+            dq.push_back(i); // Add the current index to the deque
+            
+            // The front of the deque contains the maximum element index for the current window
+            if (i >= k - 1) {
+                result.push_back(nums[dq.front()]);
+            }
+        }
+        
+        return result;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> result;
+        priority_queue<pair<int, int>> maxHeap; // Pair of (value, index)
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            // Remove elements from the maxHeap that are out of the current window
+            while (!maxHeap.empty() && maxHeap.top().second <= i - k) {
+                maxHeap.pop();
+            }
+            
+            maxHeap.push({nums[i], i}); // Add the current element to the maxHeap
+            
+            // The top of the maxHeap contains the maximum element for the current window
+            if (i >= k - 1) {
+                result.push_back(maxHeap.top().first);
+            }
+        }
+        
+        return result;
+    }
+};
+
+
+
+
+
 //CONCEPTs CRTC BUT TLE
 
 class Solution {
