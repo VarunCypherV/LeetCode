904ebddf15c9267e8f47baea3f80ac1b1e731da3
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-25 17:31:11 +0530

    Update AddTwoNumbersLL.cpp

--- AddTwoNumbersLL.cpp
+++ AddTwoNumbersLL.cpp
@@ -1,3 +1,25 @@
+// Best way
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* result = new ListNode();
+        ListNode* temp = result;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            carry = sum / 10;
+            
+            temp->next = new ListNode(sum % 10);
+            temp = temp->next;
+            
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+        
+        return result->next;
+    }
+};
+
 //Concept correct but exceeding long size for 3 test cases LOL
 class Solution {
 public:
