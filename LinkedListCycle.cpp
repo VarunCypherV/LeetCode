5434aa9fff0f3a2cf46c5e8ebe6d252a66b818d3
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-25 19:38:48 +0530

    Update LinkedListCycle.cpp

--- LinkedListCycle.cpp
+++ LinkedListCycle.cpp
@@ -1,3 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+//The fast Slow approach
+//  Initialize two pointers slow and fast to the head of the linked list.
+// Start a loop where the fast pointer moves two steps and the slow pointer moves one step.
+// If there is a cycle, the fast pointer will eventually catch up with the slow pointer.
+// If the fast pointer or its next node becomes nullptr, there is no cycle in the linked list.
+// Return true if the pointers meet (cycle detected), otherwise return false
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (!head || !head->next) {
+            return false; // No cycle if there are less than 2 nodes
+        }
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast && fast->next) {
+            if (slow == fast) {
+                return true; // Cycle detected
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return false; // No cycle found
+    }
+};
 
 
 //correct but tle 
