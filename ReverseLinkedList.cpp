eda3dc56453453a389be4832fcf0021064966d1c
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-25 13:16:26 +0530

    Update ReverseLinkedList.cpp

--- ReverseLinkedList.cpp
+++ ReverseLinkedList.cpp
@@ -23,3 +23,37 @@ public:
         return head;
     }
 };
+
+
+RECURSION IMPLEMENATION
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* recursion(ListNode* head ,ListNode* prev){
+        if(head==NULL){
+            head=prev;
+            return head;
+        }
+        ListNode* next = head->next;
+            head->next = prev; 
+            prev = head;  
+            head = next;  
+        return   recursion(head,prev);
+        
+    }
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        return recursion(head , prev);
+        
+        
+    }
+};
