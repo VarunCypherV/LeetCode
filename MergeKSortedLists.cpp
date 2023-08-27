62d71c8338e2ec1f23607dc8e4e8713e4fde2c80
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-08-27 19:22:58 +0530

    Create MergeKSortedLists.cpp

---
+++ MergeKSortedLists.cpp
@@ -0,0 +1,39 @@
+//my concept but terrible runtime lol
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode* result = new ListNode();
+        ListNode* head = result;
+        int iters = 0;
+        //mama mia get me the count of items i need to iter in total
+        for (int i = 0; i < lists.size(); i++) {
+            ListNode* current = lists[i];
+            while (current) {
+                iters++;
+                current = current->next;
+            }
+        }
+        //find the mind among all and add it
+        for (int i = 0; i < iters; i++) {
+            int minVal = INT_MAX;
+            int minIdx = -1;
+            
+            for (int j = 0; j < lists.size(); j++) {
+                if (lists[j]) {
+                    if (lists[j]->val < minVal) {
+                        minVal = lists[j]->val;
+                        minIdx = j;
+                    }
+                }
+            }
+            
+            if (minIdx != -1) {
+                result->next = new ListNode(minVal);
+                result = result->next;
+                lists[minIdx] = lists[minIdx]->next; //move the head of that particular item in list so that no dupe occurs
+            }
+        }
+
+        return head->next;
+    }
+};
