533afec5ec67fac9c62b1e2fa5738d77c1e0242b
VarunCypherV <91377728+VarunCypherV@users.noreply.github.com>
2023-10-06 17:03:28 +0530

    Update addAndSearch.cpp

--- Tries/addAndSearch.cpp
+++ Tries/addAndSearch.cpp
@@ -32,6 +32,76 @@ public:
     }
 
     bool searchInNode(string word, TrieNode* node, int index) {
+        for (int i = index; i < word.size(); i++) {
+            char c = word[i];
+            if (c == '.') {
+                for (int j = 0; j < 26; j++) {
+                    if (node->children[j] != nullptr &&
+                        searchInNode(word, node->children[j], i + 1)) {
+                        return true;
+                    }
+                }
+                return false;  // . but no word after
+            } else {
+                //regular proceedure
+                int idx = c - 'a';
+                if (node->children[idx] == nullptr) {
+                    return false; 
+                }
+                node = node->children[idx];
+            }
+        }
+        return node->eow;
+    }
+
+    bool search(string word) {
+        return searchInNode(word, root, 0);
+    }
+};
+
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
+
+
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool eow; 
+
+    TrieNode() {
+        for (int i = 0; i < 26; i++) {
+            children[i] = nullptr;
+        }
+        eow = false;
+    }
+};
+
+class WordDictionary {
+public:
+    TrieNode* root;
+
+    WordDictionary() {
+        root = new TrieNode();
+    }
+
+    void addWord(string word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (node->children[idx] == nullptr) {
+                node->children[idx] = new TrieNode();
+            }
+            node = node->children[idx];
+        }
+        node->eow = true;
+    }
+
+    bool searchInNode(string word, TrieNode* node, int index) {
         if (index == word.size()) {
             return node->eow;
         }
