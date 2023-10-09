2cafc638879b453bcdf0c12f0a6a37f0e5206402
Varun Vetrivendan <91377728+VarunCypherV@users.noreply.github.com>
2023-10-09 19:14:58 +0530

    Create KlargestElement.cpp

---
+++ HeapsAndPriorityQueue/KlargestElement.cpp
@@ -0,0 +1,61 @@
+class KthLargest {
+public:
+    int k;
+    vector<int> minHeap;
+
+    KthLargest(int k, vector<int>& nums) {
+        //intialization
+        this->k = k;
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    int add(int val) {
+        if (minHeap.size() < k) {
+            minHeap.push_back(val); //add at last then heapify
+            heapifyUp(minHeap.size() - 1);
+        } else if (val > minHeap[0]) {
+            //add at root then heapify
+            minHeap[0] = val;
+            heapifyDown(0);
+        }
+        return minHeap[0];
+    }
+
+    void heapifyUp(int i) {
+        while (i > 0) {
+            int parent = (i - 1) / 2;
+            if (minHeap[i] < minHeap[parent]) {
+                swap(minHeap[i], minHeap[parent]);
+                i = parent;
+            } else {
+                break;
+            }
+        }
+    }
+
+    void heapifyDown(int i) {
+        int n = minHeap.size();
+        while (2 * i + 1 < n) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int smallest = left;
+            if (right < n && minHeap[right] < minHeap[left]) {
+                smallest = right;
+            }
+            if (minHeap[i] > minHeap[smallest]) {
+                swap(minHeap[i], minHeap[smallest]);
+                i = smallest;
+            } else {
+                break;
+            }
+        }
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
