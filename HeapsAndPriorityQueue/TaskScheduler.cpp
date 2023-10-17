e1fb4d98942f0d9dfbbe2c196ff13a2573cc9600
Varun Vetrivendan <91377728+VarunCypherV@users.noreply.github.com>
2023-10-17 14:03:07 +0530

    Update TaskScheduler.cpp

--- HeapsAndPriorityQueue/TaskScheduler.cpp
+++ HeapsAndPriorityQueue/TaskScheduler.cpp
@@ -21,3 +21,15 @@ public:
         return tasks.size() + idleTime;
     }
 };
+
+
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        int freq[26] = {0}; int maxm = 1;  //max freq == maxm
+        for (char &ch: tasks) {freq[ch-'A'] ++; maxm = max(maxm, freq[ch-'A']);}
+        int maxcnt = 0;
+        for (int &x: freq) if (maxm == x) maxcnt++;
+        return max((int)tasks.size(), ((n+1)*(maxm-1)+maxcnt)); //typecasting (size_t) into (int).
+    }
+};
