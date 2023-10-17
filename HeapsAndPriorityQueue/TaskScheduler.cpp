a8db7119d7505761c0cd5167e4d9418141179e2c
Varun Vetrivendan <91377728+VarunCypherV@users.noreply.github.com>
2023-10-17 14:04:41 +0530

    Update TaskScheduler.cpp

--- HeapsAndPriorityQueue/TaskScheduler.cpp
+++ HeapsAndPriorityQueue/TaskScheduler.cpp
@@ -26,10 +26,13 @@ public:
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        int freq[26] = {0}; int maxm = 1;  //max freq == maxm
-        for (char &ch: tasks) {freq[ch-'A'] ++; maxm = max(maxm, freq[ch-'A']);}
-        int maxcnt = 0;
-        for (int &x: freq) if (maxm == x) maxcnt++;
+        int freq[26] = {0}; int maxm = 1;  //max freq == maxm 
+        for (char &ch: tasks) {
+            freq[ch-'A'] ++; 
+            maxm = max(maxm, freq[ch-'A']);
+        } //add in freqs and find max freq
+        int maxcnt = 0; tasks having max freq
+        for (int &x: freq) if (maxm == x) maxcnt++; //find no of items with maxfreq
         return max((int)tasks.size(), ((n+1)*(maxm-1)+maxcnt)); //typecasting (size_t) into (int).
     }
 };
